9e59663249b5310bd730c287aa8ee4e3bbe3bb8a
Fady <fae322@g.harvard.edu>
2020-01-01 22:37:55 -0800

    Initial Commit

---
+++ AtStateVariable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AtStateVariable
+//
+//  Created by Fady Eid on 1/1/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
